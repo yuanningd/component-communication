38fcf08a62190dacccbc8a665c0df2d66a4674cc
Steven Dong <yuanningd@gmail.com>
2022-11-05 09:52:52 +1100

    curry

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import Father from './component/Father';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Father />
+      <input type="text" placeholder='name' /> <br />
+      <input type="number" placeholder='age' /> <br />
+      <input type="text" placeholder='male' /> <br />
+      <input type="text" placeholder='state' /> <br />
+      <input type="number" placeholder='postcode' /> <br />
     </div>
   );
 }
